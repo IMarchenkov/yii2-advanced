614b41ab0be811775ba2c66f1d50fe72aa775db4
Igor Marchenkov <marchao@mail.ru>
2019-07-16 00:36:15 +0300

    -gii

--- api/config/main-local.php
+++ api/config/main-local.php
@@ -9,12 +9,6 @@ if (!YII_ENV_TEST) {
         'class' => 'yii\debug\Module',
         'allowedIPs' => ['*'],
     ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        'allowedIPs' => ['*'],
-    ];
 }
 
 return $config;
